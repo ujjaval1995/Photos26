e0cd5f207e8ffd6af42ac25f24c5481388e2abd7
Jishnu <jishnupatel1@gmail.com>
2020-11-13 20:09:42 -0500

    modified model

--- src/photos/model/Album.java
+++ src/photos/model/Album.java
@@ -1,6 +1,36 @@
 package photos.model;
 
-public class Album
-{
+import java.time.LocalDate;
+import java.util.*;
 
+public class Album implements Comparable<Album>
+{
+	private String name;
+	private int photo_count;
+	private LocalDate start_date;
+	private LocalDate end_date;
+	private ArrayList<Photo> photos;
+	
+	public Album(String name)
+	{
+		this.name = name;
+		photo_count = 0;
+		start_date = null;
+		end_date = null;
+		photos = new ArrayList<>();
+	}
+	
+	
+	
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+	
+	@Override
+	public int compareTo(Album album)
+	{
+		return name.compareToIgnoreCase(album.name);
+	}
 }
--- src/photos/model/User.java
+++ src/photos/model/User.java
@@ -1,40 +1,70 @@
 package photos.model;
 
-import java.util.*;
 import javafx.collections.ObservableList;
+
+import java.util.*;
 
 
 public class User implements Comparable<User>
 {
 	public static final String ADMIN = "admin";
 	
-	private String username;
-	private ObservableList<Album> obs_albums;
+	private String name;
 	private ArrayList<Album> albums;
 	private Album select_album;
-	
+	private ObservableList<Album> obs_albums;
 
-	public User(String username)
+	public User(String name)
 	{
-		this.username = username;
-		this.obs_albums = null;
-		this.albums = null;
-		this.select_album = null;
+		this.name = name;
+		albums = new ArrayList<>();
+		select_album = null;
+		obs_albums = null;
 	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public ArrayList<Album> getAlbums()
+	{
+		return albums;
+	}
+	
+	public Album getSelectedAlbum()
+	{
+		return select_album;
+	}
+	
+	// obs list
+	// complete
+	
+	public void addAlbum(String name)
+	{
+		Album album = new Album(name);
+		albums.add(album);
+	}
+	
+	public void deleteAlbum(Album album)
+	{
+		albums.remove(album);
+	}
+	
+	public void setSelectedAlbum(Album album)
+	{
+        select_album = album;
+    }
 	
 	@Override
 	public String toString()
 	{
-		return username;
+		return name;
 	}
-	
 	
 	@Override
 	public int compareTo(User user)
 	{
-		return username.compareToIgnoreCase(user.username);
+		return name.compareToIgnoreCase(user.name);
 	}
-	
-	
-	
 }
