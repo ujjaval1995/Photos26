1d5f43b9cd8dbffc16333e1201b40d72d97ecce4
Jishnu <jishnupatel1@gmail.com>
2020-11-14 14:32:07 -0500

    modified app and view

--- src/photos/app/Photos.java
+++ src/photos/app/Photos.java
@@ -2,25 +2,34 @@ package photos.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Photos extends Application
 {
+	private static BorderPane root = new BorderPane();
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception
-	{
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/photos/view/main.fxml"));
-		VBox root = (VBox) loader.load();
-		// add controller
-		// ...
-		Scene scene = new Scene(root);
+	{		
+		Parent main = FXMLLoader.load(getClass().getResource("/photos/view/main.fxml"));
+		AnchorPane login = FXMLLoader.load(getClass().getResource("/photos/view/login.fxml"));
+		
+		root.setTop(main);
+	    root.setCenter(login);
+		
 		primaryStage.setTitle("Login");
-		primaryStage.setScene(scene);
+		primaryStage.setScene(new Scene(root, 900, 625));
 		primaryStage.setResizable(false);
 		primaryStage.show();
+	}
+	
+	public static BorderPane getRoot()
+	{
+	    return root;
 	}
 
 	public static void main(String[] args)
