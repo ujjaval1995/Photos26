3d709fd78f8615479bb0cece67bf99c444d5bdde
Jishnu <jishnupatel1@gmail.com>
2020-11-15 16:11:41 -0500

    modified many things

--- src/photos/controller/AdminController.java
+++ src/photos/controller/AdminController.java
@@ -4,18 +4,28 @@ import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import photos.model.User;
+import photos.model.*;
 
 public class AdminController extends MainController
 {
 	@FXML ListView<User> userlist;
-	@FXML TextField name;
+	@FXML TextField namefield;
 	@FXML Button add;
 	@FXML Button delete;
-
-	public void doLogout()
+	
+	public void init()
 	{
-		toLogin();
+		namefield.clear();
+	}
+	
+	public void doAdd()
+	{
+		
+	}
+	
+	public void doDelete()
+	{
+		
 	}
 	
 	public void doAbout()
