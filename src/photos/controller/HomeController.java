0c82792f4d71e01976bcc9d4fca02a0d0fddef93
Jishnu <jishnupatel1@gmail.com>
2020-11-16 12:22:36 -0500

    added buttons to home controller

--- src/photos/controller/HomeController.java
+++ src/photos/controller/HomeController.java
@@ -1,10 +1,14 @@
 package photos.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import photos.model.*;
 
 public class HomeController extends MainController
@@ -16,9 +20,22 @@ public class HomeController extends MainController
 	@FXML TableColumn<Album, String> end_col;
 	
 	@FXML TextField albumfield;
+	@FXML Button add;
 	
 	@FXML DatePicker fromdate;
 	@FXML DatePicker todate;
+	@FXML Button searchbydate;
+
+	// add key 1
+	// add key 2
+	@FXML TextField value1field;
+	@FXML TextField value2field;
+	@FXML ToggleGroup group;
+	@FXML RadioButton single;
+	@FXML RadioButton and;
+	@FXML RadioButton or;
+	@FXML CheckBox createablum;
+	@FXML Button searchbytag;
 	
 	public void init()
 	{
