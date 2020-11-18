84d8f7a331d3aa937903ec7693e6173959bf5ca9
Ujjaval Shah <us89@scarletmail.rutgers.edu>
2020-11-18 11:26:16 -0500

    javadocs(photos.java) done

--- src/photos/app/Photos.java
+++ src/photos/app/Photos.java
@@ -1,3 +1,8 @@
+/**
+* This class helps with Main Application.
+* @author Jishnu Patel
+* @author Ujjaval Shah
+*/
 package photos.app;
 
 import javafx.application.Application;
@@ -6,12 +11,18 @@ import photos.controller.MainController;
 
 public class Photos extends Application
 {	
+	/**
+	 * @param primaryStage Primary stage of application
+	 * @throws Exception In case stage does not start
+	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
 		MainController.start(primaryStage);
 	}
-	
+	/**
+	 * @param args Input command line arguments
+	 */
 	public static void main(String[] args)
 	{
 		launch(args);
